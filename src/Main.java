0cc06bb3e3c6fd242d47516e8220fd8f0e21bfcb
Victor Moura <omouravictor@gmail.com>
2022-10-20 21:31:06 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int modoDeJogo = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("1 - Jogo individual");
+        System.out.println("2 - 2 jogadores");
+        System.out.printf("Digite o modo de jogo: ");
+        modoDeJogo = scan.nextInt();
+
+        if (modoDeJogo == 1) {
+
+            System.out.println("AAAAAA");
+        }else {
+
+
+        }
+
+    }
+}
